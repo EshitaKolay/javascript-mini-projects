9a3e86bd4d36e17a20ccaece2eb60d71a490b847
Eshita Kolay <kolayeshita2@gmail.com>
2026-06-24 11:57:50 +0530

    added RandomNumberGenerator mini project

---
+++ RandomNumberGenerator/RandomNum.js
@@ -0,0 +1,16 @@
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min=1;
+const max=6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1=Math.floor(Math.random()*max)+min;
+    randomNum2=Math.floor(Math.random()*max)+min;
+    randomNum3=Math.floor(Math.random()*max)+min;
+    myLabel1.textContent = randomNum1;
+    myLabel2.textContent = randomNum2;
+    myLabel3.textContent = randomNum3;
+}
